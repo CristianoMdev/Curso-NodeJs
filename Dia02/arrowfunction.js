ad28cc8154483e4f2673cf35fe95567c3d07a8f8
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-03-12 22:53:45 -0300

    atualização

---
+++ Dia02/arrowfunction.js
@@ -0,0 +1,11 @@
+function soma(x, y) {
+    return x + y
+}
+//ES5
+const minhaSoma = function (x, y) {
+    return x + y
+}
+//ES6
+const minhaDivisao = (x, y) =>  x/y
+
+console.log(minhaSoma(1,2))
---
+++ Dia02/constlet.js
@@ -0,0 +1,5 @@
+let novoNumero = 10
+novoNumero = 12
+console.log(novoNumero)
+
+const minhaConstante = "curso legal de node"
