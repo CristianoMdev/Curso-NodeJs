334ef920f5294023e620bd6c4b3c9aad7c67b918
Cristiano Medeiros <77064383+CristianoMdev@users.noreply.github.com>
2021-03-13 22:42:57 -0300

    dia 2 metade

---
+++ Dia02/desestruct.js
@@ -0,0 +1,22 @@
+/*const pessoa = {
+    nome:'Cristiano',
+    idade: 33,
+    email:'cristianomdev@gmail.com',
+    profissao:'desenvolvedor' 
+}
+
+let profissao = pessoa
+
+console.log(profissao)*/
+
+const pessoa = {
+    nome:'Cristiano',
+    idade: 33,
+    email:'cristianomdev@gmail.com',
+    profissao:'desenvolvedor' 
+}
+
+let{ profissao, idade }= pessoa
+
+console.log(profissao, idade)
+
---
+++ Dia02/templatestring.js
@@ -0,0 +1,12 @@
+const pessoa = {
+nome:'Cristiano Medeiros',
+email:'cristianomdev@gmail.com'
+
+}
+let {nome, email } = pessoa
+
+console.log('Olá, meu nome é ' + pessoa.nome + '. Meu e-mail é ' + pessoa.email + '. Conte comigo na sua jornada')
+
+console.log(`Olá, meu nome é ${pessoa.nome}. Meu e-mail é ${pessoa.email}. Conte comigo na sua jornada`)
+
+console.log(`Olá, meu nome é ${nome}. Meu e-mail é ${email}. Conte comigo na sua jornada`)
